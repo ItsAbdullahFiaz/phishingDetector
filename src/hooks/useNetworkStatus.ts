ca10d4483f585314f946b88aa55afef5721b31b3
Abdullah <abdullahfiaz@hwryk.com>
2024-03-31 03:09:49 +0500

    NetInfo package installed and hook created

---
+++ src/hooks/useNetworkStatus.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import NetInfo from "@react-native-community/netinfo";
+
+const useNetworkStatus = () => {
+  const [isInternetConnected, setIsInternetConnected] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsInternetConnected(state.isConnected);
+    });
+
+    NetInfo.fetch().then((state) => {
+      setIsInternetConnected(state.isConnected);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return isInternetConnected;
+};
+
+export default useNetworkStatus;
